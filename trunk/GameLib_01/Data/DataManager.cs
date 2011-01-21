cb364736aadc85e624b1af45194d8d32a3f2ecb5
ddboarm@gmail.com <ddboarm@gmail.com@86a7001c-9d32-3995-c6aa-85bc32f84ac4>
2011-01-21 13:31:52 +0000

    1/21/2011:dB:Changed direction on reading data. Using .CSV format for early stages. GameLib_01.Data.DataManager created and added to project. Also, strategem_01 has an App.config started.

---
+++ trunk/GameLib_01/Data/DataManager.cs
@@ -0,0 +1,71 @@
+﻿#region Using definitions
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+// 
+
+//
+using Engine_01.Runtime; 
+#endregion
+
+namespace GameLib_01.Data
+{
+    /// <summary>
+    /// DataManager
+    /// 
+    ///     TODO: Add class comments
+    ///     
+    /// currently working as an instance class although may not be instanced
+    /// at all since I am going to provide public static methods for now - may
+    /// be internally instanced - not sure at this point
+    /// </summary>
+    public class DataManager
+    {
+        #region Fields
+        private static EngineClock _clock = EngineClock.Clock;
+        #endregion
+
+        #region Init
+        //  Include an init section only in objects where a constructor
+        //  is defined.
+        #endregion
+ 
+        #region Functions
+        public static bool LoadDBContent ( FileInfo DataFile )
+        {
+            object RowSetObj;
+            object ConnectToDatabase;
+
+            if (File.Exists ( DataFile.FullName ))
+            {
+                readFile ( DataFile );
+            }
+
+            return false;
+        }
+
+        //  =======================================================
+        //  private functions
+        private static bool readFile ( FileInfo dataFile )
+        {
+            bool _readSuccess = false;
+
+            using (Stream stream = new FileStream ( dataFile.OpenRead ( ).SafeFileHandle, FileAccess.Read ))
+            {
+                _readSuccess = true;
+            }
+
+            return _readSuccess;
+        }
+        #endregion
+
+        #region Properties
+        //  Include a properies section only in objects where properties
+        //  are defined.
+        #endregion
+   }
+
+}
