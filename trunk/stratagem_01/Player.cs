13056e76e3145c7882c8e1733a9989b6934c982d
ddboarm@gmail.com <ddboarm@gmail.com@86a7001c-9d32-3995-c6aa-85bc32f84ac4>
2011-01-23 13:26:52 +0000

    1/23/2011:dB:Uploaded Player.cs.

---
+++ trunk/stratagem_01/Player.cs
@@ -0,0 +1,80 @@
+﻿#region Using definitions
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//
+#endregion
+
+namespace Stratagem
+{
+    /// <summary>
+    /// ClassTemplate
+    /// 
+    /// </summary>
+    public class Player
+    {
+        #region Fields
+        private Cell[] cells;
+        private string name;
+        private float credits;
+        private float worth;
+        #endregion
+
+        #region Init
+        public Player ( Cell[] Cells, String Name, float Credits, float Worth)
+        {
+            cells = Cells;
+            name = Name;
+            credits = Credits;
+            worth = Worth;
+        }
+        #endregion
+ 
+        #region Functions
+        public Cell GetCell ( int CellNumber )
+        {
+            return cells[ CellNumber ];
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public float Credits
+        {
+            get
+            {
+                return credits;
+            }
+            set
+            {
+                credits = value;
+            }
+        }
+
+        public float Worth
+        {
+            get
+            {
+                return worth;
+            }
+            set
+            {
+                worth = value;
+            }
+        }
+
+        #endregion
+   }
+}
